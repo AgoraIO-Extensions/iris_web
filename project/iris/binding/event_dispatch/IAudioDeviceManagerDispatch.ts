e53cbaad72326945235ceb9fea197af5770842ee
jindotori <jseun112@naver.com>
2023-09-04 17:49:26 +0900

    All parameters of IAudioDeviceManagerDispatch is changed to apiParam

--- project/iris/binding/event_dispatch/IAudioDeviceManagerDispatch.ts
+++ project/iris/binding/event_dispatch/IAudioDeviceManagerDispatch.ts
@@ -1,3 +1,4 @@
+import { ApiParam } from "../../engine/IrisApiEngine";
 import { IrisRtcEngine } from "../../engine/IrisRtcEngine";
 import { AudioDeviceManagerImpl } from "../../impl/AudioDeviceManagerImpl";
 import { IAudioDeviceManager } from "../interface/IAudioDeviceManager";
@@ -11,236 +12,194 @@ export class IAudioDeviceManagerDispatch {
     }
 
     enumeratePlaybackDevices(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
+        apiParam: ApiParam): number {
         // let obj = JSON.parse(params) as any;
-        result.result = this._impl.enumeratePlaybackDevices();
+        // result.result = this._impl.enumeratePlaybackDevices();
         return 0;
     }
 
     enumerateRecordingDevices(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
+        apiParam: ApiParam): number {
         // let obj = JSON.parse(params) as any;
-        result.result = this._impl.enumerateRecordingDevices();
+        // result.result = this._impl.enumerateRecordingDevices();
         return 0;
     }
 
     setPlaybackDevice(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
-        let obj = JSON.parse(params) as any;
+        apiParam: ApiParam): number {
+        let obj = JSON.parse(apiParam.data) as any;
         let deviceId = obj.deviceId;
         if (deviceId === undefined) throw "deviceId is undefined";
-        result.result = this._impl.setPlaybackDevice(deviceId);
-        return 0;
+        return this._impl.setPlaybackDevice(deviceId);
     }
 
     getPlaybackDevice(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
-        let obj = JSON.parse(params) as any;
+        apiParam: ApiParam): number {
+        let obj = JSON.parse(apiParam.data) as any;
         // let deviceId = obj.deviceId;
         // if (deviceId === undefined) throw "deviceId is undefined";
-        result.result = this._impl.getPlaybackDevice();
+        // result.result = this._impl.getPlaybackDevice();
         return 0;
     }
 
     getPlaybackDeviceInfo(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
+        apiParam: ApiParam): number {
         // let obj = JSON.parse(params) as any;
         // let deviceId = obj.deviceId;
         // if (deviceId === undefined) throw "deviceId is undefined";
         // let deviceName = obj.deviceName;
         // if (deviceName === undefined) throw "deviceName is undefined";
-        result.result = this._impl.getPlaybackDeviceInfo();
+        // result.result = this._impl.getPlaybackDeviceInfo();
         return 0;
     }
 
     setPlaybackDeviceVolume(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
-        let obj = JSON.parse(params) as any;
+        apiParam: ApiParam): number {
+        let obj = JSON.parse(apiParam.data) as any;
         let volume = obj.volume;
         if (volume === undefined) throw "volume is undefined";
-        result.result = this._impl.setPlaybackDeviceVolume(volume);
-        return 0;
+        return this._impl.setPlaybackDeviceVolume(volume);
     }
 
     getPlaybackDeviceVolume(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
-        let obj = JSON.parse(params) as any;
+        apiParam: ApiParam): number {
+        let obj = JSON.parse(apiParam.data) as any;
         // let volume = obj.volume;
         // if (volume === undefined) throw "volume is undefined";
-        result.result = this._impl.getPlaybackDeviceVolume();
-        return 0;
+        return this._impl.getPlaybackDeviceVolume();
     }
 
     setRecordingDevice(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
-        let obj = JSON.parse(params) as any;
+        apiParam: ApiParam): number {
+        let obj = JSON.parse(apiParam.data) as any;
         let deviceId = obj.deviceId;
         if (deviceId === undefined) throw "deviceId is undefined";
-        result.result = this._impl.setRecordingDevice(deviceId);
-        return 0;
+        return this._impl.setRecordingDevice(deviceId);
     }
 
     getRecordingDevice(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
-        let obj = JSON.parse(params) as any;
+        apiParam: ApiParam): number {
+        let obj = JSON.parse(apiParam.data) as any;
         // let deviceId = obj.deviceId;
         // if (deviceId === undefined) throw "deviceId is undefined";
-        result.result = this._impl.getRecordingDevice();
+        // result.result = this._impl.getRecordingDevice();
         return 0;
     }
 
     getRecordingDeviceInfo(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
-        let obj = JSON.parse(params) as any;
+        apiParam: ApiParam): number {
+        let obj = JSON.parse(apiParam.data) as any;
         // let deviceId = obj.deviceId;
         // if (deviceId === undefined) throw "deviceId is undefined";
         // let deviceName = obj.deviceName;
         // if (deviceName === undefined) throw "deviceName is undefined";
-        result.result = this._impl.getRecordingDeviceInfo();
+        // result.result = this._impl.getRecordingDeviceInfo();
         return 0;
     }
 
     setRecordingDeviceVolume(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
-        let obj = JSON.parse(params) as any;
+        apiParam: ApiParam): number {
+        let obj = JSON.parse(apiParam.data) as any;
         let volume = obj.volume;
         if (volume === undefined) throw "volume is undefined";
-        result.result = this._impl.setRecordingDeviceVolume(volume);
-        return 0;
+        return this._impl.setRecordingDeviceVolume(volume);
     }
 
     getRecordingDeviceVolume(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
-        let obj = JSON.parse(params) as any;
+        apiParam: ApiParam): number {
+        let obj = JSON.parse(apiParam.data) as any;
         // let volume = obj.volume;
         // if (volume === undefined) throw "volume is undefined";
-        result.result = this._impl.getRecordingDeviceVolume();
-        return 0;
+        return this._impl.getRecordingDeviceVolume();
     }
 
     setPlaybackDeviceMute(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
-        let obj = JSON.parse(params) as any;
+        apiParam: ApiParam): number {
+        let obj = JSON.parse(apiParam.data) as any;
         let mute = obj.mute;
         if (mute === undefined) throw "mute is undefined";
-        result.result = this._impl.setPlaybackDeviceMute(mute);
-        return 0;
+        return this._impl.setPlaybackDeviceMute(mute);
     }
 
     getPlaybackDeviceMute(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
-        let obj = JSON.parse(params) as any;
+        apiParam: ApiParam): number {
+        let obj = JSON.parse(apiParam.data) as any;
         let mute = obj.mute;
         if (mute === undefined) throw "mute is undefined";
-        result.result = this._impl.getPlaybackDeviceMute(mute);
-        return 0;
+        return this._impl.getPlaybackDeviceMute(mute);
     }
 
     setRecordingDeviceMute(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
-        let obj = JSON.parse(params) as any;
+        apiParam: ApiParam): number {
+        let obj = JSON.parse(apiParam.data) as any;
         let mute = obj.mute;
         if (mute === undefined) throw "mute is undefined";
-        result.result = this._impl.setRecordingDeviceMute(mute);
-        return 0;
+        return this._impl.setRecordingDeviceMute(mute);
     }
 
     getRecordingDeviceMute(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
-        let obj = JSON.parse(params) as any;
+        apiParam: ApiParam): number {
+        let obj = JSON.parse(apiParam.data) as any;
         let mute = obj.mute;
         if (mute === undefined) throw "mute is undefined";
-        result.result = this._impl.getRecordingDeviceMute(mute);
-        return 0;
+        return this._impl.getRecordingDeviceMute(mute);
     }
 
     startPlaybackDeviceTest(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
-        let obj = JSON.parse(params) as any;
+        apiParam: ApiParam): number {
+        let obj = JSON.parse(apiParam.data) as any;
         let testAudioFilePath = obj.testAudioFilePath;
         if (testAudioFilePath === undefined) throw "testAudioFilePath is undefined";
-        result.result = this._impl.startPlaybackDeviceTest(testAudioFilePath);
-        return 0;
+        return this._impl.startPlaybackDeviceTest(testAudioFilePath);
     }
 
     stopPlaybackDeviceTest(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
-        let obj = JSON.parse(params) as any;
-        result.result = this._impl.stopPlaybackDeviceTest();
-        return 0;
+        apiParam: ApiParam): number {
+        let obj = JSON.parse(apiParam.data) as any;
+        return this._impl.stopPlaybackDeviceTest();
     }
 
     startRecordingDeviceTest(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
-        let obj = JSON.parse(params) as any;
+        apiParam: ApiParam): number {
+        let obj = JSON.parse(apiParam.data) as any;
         let indicationInterval = obj.indicationInterval;
         if (indicationInterval === undefined) throw "indicationInterval is undefined";
-        result.result = this._impl.startRecordingDeviceTest(indicationInterval);
-        return 0;
+        return this._impl.startRecordingDeviceTest(indicationInterval);
     }
 
     stopRecordingDeviceTest(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
-        let obj = JSON.parse(params) as any;
-        result.result = this._impl.stopRecordingDeviceTest();
-        return 0;
+        apiParam: ApiParam): number {
+        let obj = JSON.parse(apiParam.data) as any;
+        return this._impl.stopRecordingDeviceTest();
     }
 
     startAudioDeviceLoopbackTest(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
-        let obj = JSON.parse(params) as any;
+        apiParam: ApiParam): number {
+        let obj = JSON.parse(apiParam.data) as any;
         let indicationInterval = obj.indicationInterval;
         if (indicationInterval === undefined) throw "indicationInterval is undefined";
-        result.result = this._impl.startAudioDeviceLoopbackTest(indicationInterval);
-        return 0;
+        return this._impl.startAudioDeviceLoopbackTest(indicationInterval);
     }
 
     stopAudioDeviceLoopbackTest(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
-        let obj = JSON.parse(params) as any;
-        result.result = this._impl.stopAudioDeviceLoopbackTest();
-        return 0;
+        apiParam: ApiParam): number {
+        let obj = JSON.parse(apiParam.data) as any;
+        return this._impl.stopAudioDeviceLoopbackTest();
     }
 
     followSystemPlaybackDevice(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
-        let obj = JSON.parse(params) as any;
+        apiParam: ApiParam): number {
+        let obj = JSON.parse(apiParam.data) as any;
         let enable = obj.enable;
         if (enable === undefined) throw "enable is undefined";
-        result.result = this._impl.followSystemPlaybackDevice(enable);
-        return 0;
+        return this._impl.followSystemPlaybackDevice(enable);
     }
 
     followSystemRecordingDevice(
-        params: string, paramLength: number,
-        buffer: Array<Uint8ClampedArray>, bufferLength: number, result: any): number {
-        let obj = JSON.parse(params) as any;
+        apiParam: ApiParam): number {
+        let obj = JSON.parse(apiParam.data) as any;
         let enable = obj.enable;
         if (enable === undefined) throw "enable is undefined";
-        result.result = this._impl.followSystemRecordingDevice(enable);
-        return 0;
+        return this._impl.followSystemRecordingDevice(enable);
     }
 }
