975b77f4f2bdd7ec79e2906880b3195e0e5bb3f4
gxz <guoxianzhe@agora.io>
2023-09-15 13:38:28 +0800

    11

---
+++ packages/rtc/test/impl/IAgoraRtcEngineImpl.test.ts
@@ -0,0 +1,8 @@
+import { AgoraWrapper } from "../../src";
+import { expect } from "vitest";
+
+describe("useConnectionState", () => {
+  test("joinChannel", () => {
+//call create engine
+  });
+});
---
+++ packages/rtc/test/setup.tsx
@@ -0,0 +1,33 @@
+import "@testing-library/jest-dom";
+import { expect, vi } from "vitest";
+
+expect.extend(matchers);
+
+/**
+ * started agora-rtc-sdk-ng@17.0.0, need mock RTCPeerConnection.
+ * RTCPeerConnection does not implement global
+ */
+global.RTCPeerConnection = vi.fn();
+
+/**
+ * JSDOM does not implement global "HTMLMediaElement.prototype.play" function
+ */
+HTMLMediaElement.prototype.play = vi.fn().mockReturnValue(Promise.resolve());
+HTMLMediaElement.prototype.pause = vi.fn().mockReturnValue(Promise.resolve());
+
+/**
+ * navigator does not implement global "mediaDevices.prototype.getUserMedia" function
+ * navigator does not implement global "mediaDevices.prototype.enumerateDevices" function
+ *
+ */
+const mockPromise = vi.fn(async () => {
+  return new Promise<void>(resolve => {
+    resolve();
+  });
+});
+Object.defineProperty(global.navigator, "mediaDevices", {
+  value: {
+    getUserMedia: mockPromise,
+    enumerateDevices: mockPromise,
+  },
+});
